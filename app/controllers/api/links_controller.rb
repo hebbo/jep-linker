270b5435aba83e736eb0d57dbcdb55ee49078ce0
Angel Ruiz <angel@cultureamp.com>
2018-03-08 18:53:03 +1100

    accept token

--- app/controllers/api/links_controller.rb
+++ app/controllers/api/links_controller.rb
@@ -1,5 +1,33 @@
 class Api::LinksController < ApplicationController
+  before_action :authenticate_user_from_token!
+
   def index
-    render json: Link.all.map { |link| { id: link.id.to_s, long_url: link.long_url, short_url: link.short_url } }
+    if user_signed_in?
+      render json: Link
+                    .where(user_id: current_user.id.to_s)
+                    .map { |link| { id: link.id.to_s, long_url: link.long_url, short_url: link.short_url } }
+    else
+      render json: Link
+                    .all
+                    .map { |link| { id: link.id.to_s, long_url: link.long_url, short_url: link.short_url } }
+    end
+  end
+
+  private
+
+  # To make authentication mechanism more safe,
+  # require an access_token
+  def authenticate_user_from_token!
+
+    # Use Devise.secure_compare to compare the access_token
+    # in the database with the access_token given in the params.
+    user = User.find_by(access_token: params[:access_token])
+    if user
+      # Passing store false, will not store the user in the session,
+      # so an access_token is needed for every request.
+      # If you want the access_token to work as a sign in token,
+      # you can simply remove store: false.
+      sign_in user, store: false
+    end
   end
 end
--- spec/controllers/api/links_controller_spec.rb
+++ spec/controllers/api/links_controller_spec.rb
@@ -2,27 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Api::LinksController, type: :controller do
   describe '#index' do
-    let!(:link1) { FactoryBot.create(:link) }
-    let!(:link2) { FactoryBot.create(:link) }
-    let(:expected_links) do
-      [
-        {
-          'id' => link1.id.to_s,
-          'long_url' => link1.long_url,
-          'short_url' => link1.short_url,
-        },
-        {
-          'id' => link2.id.to_s,
-          'long_url' => link2.long_url,
-          'short_url' => link2.short_url,
-        },
-      ]
+    context 'no token provided' do
+      let!(:link1) { FactoryBot.create(:link) }
+      let!(:link2) { FactoryBot.create(:link) }
+      let(:expected_links) do
+        [
+          {
+            'id' => link1.id.to_s,
+            'long_url' => link1.long_url,
+            'short_url' => link1.short_url,
+          },
+          {
+            'id' => link2.id.to_s,
+            'long_url' => link2.long_url,
+            'short_url' => link2.short_url,
+          },
+        ]
+      end
+
+      it 'returns all links' do
+        get :index
+        expect(response).to be_ok
+        expect(response.body).to eq(expected_links.to_json)
+      end
     end
 
-    it 'returns all links' do
-      get :index
-      expect(response).to be_ok
-      expect(response.body).to eq(expected_links.to_json)
+    context 'token is provided' do
+      let(:user) { FactoryBot.create(:user) }
+      let!(:link1) { FactoryBot.create(:link) }
+      let!(:link2) { FactoryBot.create(:link) }
+      let!(:anonymous_link) { FactoryBot.create(:link) }
+      let(:expected_links) do
+        [
+          {
+            'id' => link1.id.to_s,
+            'long_url' => link1.long_url,
+            'short_url' => link1.short_url,
+          },
+          {
+            'id' => link2.id.to_s,
+            'long_url' => link2.long_url,
+            'short_url' => link2.short_url,
+          },
+        ]
+      end
+
+      it 'returns the user links' do
+        user.links << link1
+        user.links << link2
+
+        get :index, params: { access_token: user.access_token }
+
+        expect(response).to be_ok
+        expect(response.body).to eq(expected_links.to_json)
+      end
     end
   end
 end
